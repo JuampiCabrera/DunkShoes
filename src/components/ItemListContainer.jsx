1687b3a9f9039fc70dad20993725ced27851ff9f
Juampi Cabrera <juampibariloche@gmail.com>
2024-10-28 22:28:24 -0300

    ajustes 2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect} from "react"
 import { useParams } from "react-router-dom"
-import ItemList from './Itemlist'
+import ItemList from './ItemList'
 
 function ItemListContainer() {
     const [items, setItems]= useState([])
